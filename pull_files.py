21870f98f488c75fd1e78cf51a41b2feb7784b6e
Ben Hughes <benjamin.b.hughes@gmail.com>
2017-10-04 09:38:23 -0700

    Add json template, and modify python script

--- pull_files.py
+++ pull_files.py
@@ -3,22 +3,24 @@ import urllib.request
 
 from pprint import pprint
 
-with open("data.json") as open_file:
+with open("file_props.json") as open_file:
     data = json.load(open_file)
+
+group_dir = 'importables'
 
 for blob in data:
     with urllib.request.urlopen(blob['file']) as response:
         doc_file = response.read()
-    with open(blob['title']+'.md', 'wb') as doc_out:
+    with open(group_dir + blob['meta']['title']+'.md', 'wb') as doc_out:
         doc_out.write(doc_file)
 
-    with open(blob['title']+'.md.meta', 'w') as doc_meta:
-        doc_meta.write("title: " + blob['title'])
-        doc_meta.write("\n") # we expect python to convert to os line-ending
-        doc_meta.write("tags: " + blob['tags'])
+    with open(group_dir + blob['meta']['title']+'.md.meta', 'w') as doc_meta:
+        for prop in blob['meta']:
+            doc_meta.write(prop + ': ' + blob['meta'][prop])
+            doc_meta.write("\n") # we expect python to convert to os line-ending
 
-    with open('_importables', 'w') as importables_list:
+    with open(group_dir + '_importables', 'w') as importables_list:
         importables_list.truncate()
-        importables_list.write(blob['title']+'.md'+'\n')
+        importables_list.write(blob['meta']['title']+'.md'+'\n')
 
 
