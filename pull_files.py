6463ee8114d32d4d9e981179fe52e781f3a8e287
Ben Hughes <benjamin.b.hughes@gmail.com>
2017-10-04 09:51:16 -0700

    Put downloaded files in a directory
    Files downloaded and created by this script are now written to the
    <importables> subdirectory of the location where this script is run.

--- pull_files.py
+++ pull_files.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+import os
 
 from pprint import pprint
 
@@ -7,20 +8,26 @@ with open("file_props.json") as open_file:
     data = json.load(open_file)
 
 group_dir = 'importables'
+try:
+    os.mkdir('./importables')
+except FileExistsError:
+    print("importables exists")
+
+os.chdir('./importables')
 
 for blob in data:
     with urllib.request.urlopen(blob['file']) as response:
         doc_file = response.read()
-    with open(group_dir + blob['meta']['title']+'.md', 'wb') as doc_out:
+    with open(blob['meta']['title']+'.md', 'wb') as doc_out:
         doc_out.write(doc_file)
 
-    with open(group_dir + blob['meta']['title']+'.md.meta', 'w') as doc_meta:
+    with open(blob['meta']['title']+'.md.meta', 'w') as doc_meta:
         for prop in blob['meta']:
             doc_meta.write(prop + ': ' + blob['meta'][prop])
             doc_meta.write("\n") # we expect python to convert to os line-ending
 
-    with open(group_dir + '_importables', 'w') as importables_list:
+    with open('_importables', 'w') as importables_list:
         importables_list.truncate()
-        importables_list.write(blob['meta']['title']+'.md'+'\n')
+        importables_list.write(group_dir + '/' + blob['meta']['title']+'.md'+'\n')
 
 
